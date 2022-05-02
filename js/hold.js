7764b2b718390dfcd4cc81ca054ee48d9df799a2
melvinjim <melvinjimenezang@hotmail.com>
2022-05-02 13:36:48 -0500

    adjustments in the code for the execution of the results

--- js/hold.js
+++ js/hold.js
@@ -1,157 +1,76 @@
 document.getElementById('hold').addEventListener('click', function() {
-    var firstFace = document.getElementById("dices");
-    firstFace.classList.remove("none");
+    var dices = document.getElementById("dices");
+    dices.classList.remove("none");
 
-    var secondFace = document.getElementById("image-gif");
-    secondFace.classList.remove("flex");
+    var images = document.getElementById("image-gif");
+    images.classList.remove("flex");
 
+    const boxes = document.getElementById("dices").getElementsByTagName("div");
+    for (const box of boxes) {
+        box.classList.remove('visibility');
+    }
+
+    printDice("left-dice");
+    printDice("right-dice");
+});
+
+printDice = function(diceID,random){
     var min = 1;
     var max = 7;
-    var firstNumber = Math.floor(Math.random() * (+max - +min)) + +min; 
-    console.log(firstNumber);
+    random = Math.floor(Math.random() * (+max - +min)) + +min;
 
-    var min = 1;
-    var max = 7;
-    var secondNumber = Math.floor(Math.random() * (+max - +min)) + +min;
-    console.log(secondNumber);
+    if (diceID == "left-dice"){
+        ramdomLeft = random;
+    }
 
-    var firstRow = document.getElementsByClassName("first-row");
+    document.getElementById("result").innerHTML = ramdomLeft + random;
+
+    var dice = document.getElementById(diceID);
+
+    var firstRow = dice.getElementsByClassName("first-row");
     firstRow = firstRow[0];
 
-    var secondRow = document.getElementsByClassName("second-row");
+    var secondRow = dice.getElementsByClassName("second-row");
     secondRow = secondRow[0];
 
-    var thirdRow = document.getElementsByClassName("third-row");
+    var thirdRow = dice.getElementsByClassName("third-row");
     thirdRow = thirdRow[0];
 
-    var firstRowRight = document.getElementsByClassName("first-row-right");
-    firstRowRight = firstRowRight[0];
-
-    var secondRowRight = document.getElementsByClassName("second-row-right");
-    secondRowRight = secondRowRight[0];
-
-    var thirdRowRight = document.getElementsByClassName("third-row-right");
-    thirdRowRight = thirdRowRight[0];
-
     var firstBall = firstRow.getElementsByClassName("first-ball");
-    var firstballRight = firstRowRight.getElementsByClassName("first-ball");
-    for (x=0;x<firstBall.length;x++){
-        if(firstNumber == 2 || firstNumber == 4 || firstNumber == 6 || firstNumber == 5){
-            firstBall[x].classList.add("visibility");
-        } else {
-            firstBall[x].classList.remove("visibility");
-        }
-        for (x=0;x<firstballRight.length;x++){
-            if(secondNumber == 2 || secondNumber == 4 || secondNumber == 6 || secondNumber == 5){
-                firstballRight[x].classList.add("visibility");
-            } else {
-                firstballRight[x].classList.remove("visibility");
-            }
-        }
+    if(random == 2 || random == 4 || random == 6 || random == 5){
+        firstBall[0].classList.add("visibility");
     }
 
     var secondBall = secondRow.getElementsByClassName("first-ball");
-    var secondBallRight = secondRowRight.getElementsByClassName("first-ball");
-    for (x=0;x<secondBall.length;x++){
-        if(firstNumber == 6){
-            secondBall[x].classList.add("visibility");
-        } else {
-            secondBall[x].classList.remove("visibility");
-        }
-        for (x=0;x<secondBallRight.length;x++){
-            if(secondNumber == 6){
-                secondBallRight[x].classList.add("visibility");
-            } else {
-                secondBallRight[x].classList.remove("visibility");
-            }
-        }
+    if(random == 6){
+        secondBall[0].classList.add("visibility");
     }
 
     var thirdBall = thirdRow.getElementsByClassName("first-ball");
-    var thirdBallRight = thirdRowRight.getElementsByClassName("first-ball");
-    for (x=0;x<thirdBall.length;x++){
-        if(firstNumber == 4 || firstNumber == 6 || firstNumber == 5 || firstNumber == 3){
-            thirdBall[x].classList.add("visibility");
-        } else {
-            thirdBall[x].classList.remove("visibility");
-        }
-        for (x=0;x<thirdBallRight.length;x++){
-            if(secondNumber == 4 || secondNumber == 6 || secondNumber == 5 || secondNumber == 3){
-                thirdBallRight[x].classList.add("visibility");
-            } else {
-                thirdBallRight[x].classList.remove("visibility");
-            }
-        }
-    }
+    if(random == 4 || random == 6 || random == 5 || random == 3){
+        thirdBall[0].classList.add("visibility");
+    } 
 
     var secondBall = secondRow.getElementsByClassName("second-ball");
-    secondBallRight = secondRowRight.getElementsByClassName("second-ball");
-    for (x=0;x<secondBall.length;x++){
-        if(firstNumber == 1 ||firstNumber == 5 || firstNumber == 3){
-            secondBall[x].classList.add("visibility");
-        } else {
-            secondBall[x].classList.remove("visibility");
-        }
-        for (x=0;x<secondBallRight.length;x++){
-            if(secondNumber == 1 || secondNumber == 5 || secondNumber == 3){
-                secondBallRight[x].classList.add("visibility");
-            } else {
-                secondBallRight[x].classList.remove("visibility");
-            }
-        }
-    }
+    if(random == 1 ||random == 5 || random == 3){
+        secondBall[0].classList.add("visibility");
+    } 
 
     var firstBall = firstRow.getElementsByClassName("third-ball");
-    var firstballRight = firstRowRight.getElementsByClassName("third-ball")
-    for (x=0;x<firstBall.length;x++){
-        if(firstNumber == 3 || firstNumber == 4 || firstNumber == 6 || firstNumber == 5){
-            firstBall[x].classList.add("visibility");
-        } else {
-            firstBall[x].classList.remove("visibility");
-        }
-        for (x=0;x<firstballRight.length;x++){
-            if(secondNumber == 3 || secondNumber == 4 || secondNumber == 6 || secondNumber == 5){
-                firstballRight[x].classList.add("visibility");
-            } else {
-                firstballRight[x].classList.remove("visibility");
-            }
-        }
-    }
+    if(random == 3 || random == 4 || random == 6 || random == 5){
+        firstBall[0].classList.add("visibility");
+    } 
 
     var secondBall = secondRow.getElementsByClassName("third-ball");
-    var secondBallRight = secondRowRight.getElementsByClassName("third-ball");
-    for (x=0;x<secondBall.length;x++){
-        if(firstNumber == 6){
-            secondBall[x].classList.add("visibility");
-        } else {
-            secondBall[x].classList.remove("visibility");
-        } 
-        for (x=0;x<secondBallRight.length;x++){
-            if(secondNumber == 6){
-                secondBallRight[x].classList.add("visibility");
-            } else {
-                secondBallRight[x].classList.remove("visibility");
-            }
-        }
-    }
+    if(random == 6){
+        secondBall[0].classList.add("visibility");
+    } 
 
     var thirdBall = thirdRow.getElementsByClassName("third-ball");
-    var thirdBallRight = thirdRowRight.getElementsByClassName("third-ball");
-    for (x=0;x<thirdBall.length;x++){
-        if(firstNumber == 2 || firstNumber == 4 || firstNumber == 6 || firstNumber == 5){
-            thirdBall[x].classList.add("visibility");
-        } else {
-            thirdBall[x].classList.remove("visibility");
-        }
-        for (x=0;x<thirdBallRight.length;x++){
-            if(secondNumber == 2 || secondNumber == 4 || secondNumber == 6 || secondNumber == 5){
-                thirdBallRight[x].classList.add("visibility");
-            } else {
-                thirdBallRight[x].classList.remove("visibility");
-            }
-        }
+    if(random == 2 || random == 4 || random == 6 || random == 5){
+        thirdBall[0].classList.add("visibility");
     }
 
-    var totalResult = firstNumber + secondNumber;
-    document.getElementById("result").innerHTML = totalResult;
-});
+    // document.getElementById("result").innerHTML = random;
+    // return random;    
+}
--- js/roll_dice.js
+++ js/roll_dice.js
@@ -1,7 +1,12 @@
+var buttonDisable = document.getElementById("hold")
+buttonDisable.classList.add("disabled-button")
+
 document.getElementById('roll').addEventListener('click', function() {
     var firstFace = document.getElementById("dices");
     firstFace.classList.add("none");
 
     var secondFace = document.getElementById("image-gif");
     secondFace.classList.add("flex");
+
+    buttonDisable.classList.remove("disabled-button")
 });
